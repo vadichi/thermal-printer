d5f7c4428e5ae8f5012f40212a6197cd422ec215
Vadim Chichikalyuk <chichikalyuk@gmail.com>
2023-04-28 22:44:56 +0100

    Added test client

---
+++ TestClient/index.js
@@ -0,0 +1,13 @@
+const socket = new WebSocket('ws://green-frost-printer.local:8080/');
+socket.binaryType = 'arraybuffer';
+
+const encoder = new TextEncoder();
+const decoder = new TextDecoder();
+
+socket.addEventListener('open', (event) => {
+    console.log('Connection established');
+})
+
+socket.addEventListener('message', (event) => {
+    console.log('Received message: ', decoder.decode(event.data));
+})
